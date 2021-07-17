17ad9f66da110d1936717e87577e180050caaf35
prestidge.amanda <prestidge.amanda@gmail.com>
2021-07-17 16:52:27 -0400

    can get basic directions

--- src/just_the_recipe.py
+++ src/just_the_recipe.py
@@ -44,17 +44,36 @@ def get_recipe_title(html):
     title_content = get_tag_content(html,'title')
     print(title_content)
 
-def get_ingredient_list_with_soup(html):
+def get_page_text(html):
     soup_html = BeautifulSoup(html, "html.parser")
     page_text = soup_html.get_text()
-    ingredients_start = page_text.find('Ingredients')
+    return page_text
+
+def get_ingredient_list(page_text):
+    ingredients_start = page_text.find('Ingredients') # multiple or none case needs to be handled
     directions_start = page_text.find('Directions')
+    if(directions_start == -1):
+        directions_start= page_text.find('Steps')
     ingredients_content_raw =  page_text[ingredients_start:directions_start]
     ingredients_content =  re.sub("   *", "\n", ingredients_content_raw)
     print(ingredients_content)
+
+def get_directions(page_text):
+    directions_start= page_text.find('Directions')
+    directions_text = page_text[directions_start:]
+    comments_start = directions_text.find('Comments')
+    reviews_start = directions_text.find('Reviews')
+    if (comments_start != -1):
+        directions_text = directions_text[:comments_start]
+    elif (reviews_start != -1):
+        directions_text = directions_text[:reviews_start]
+    directions_content = re.sub("   *", "\n", directions_text)
+    print(directions_content)
 
 url = get_url()
 html = get_html(url)
 if(html != ""):
     get_recipe_title(html)
-    get_ingredient_list_with_soup(html)
+    page_text = get_page_text(html)
+    get_ingredient_list(page_text)
+    get_directions(page_text)
