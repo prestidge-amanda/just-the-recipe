e4ed414fd361f43bc00a89de7156de903d9fd65e
prestidge.amanda <prestidge.amanda@gmail.com>
2021-07-17 15:53:01 -0400

    can get ugly ingredient list

--- src/get_url.py
+++
@@ -1,2 +0,0 @@
-url = input('Enter url for recipe blog: ')
-print(url)
--- src/just_the_recipe.py
+++ src/just_the_recipe.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+import re
 from urllib.request import urlopen
 
 def get_url():
@@ -12,19 +14,32 @@ def get_tag_content(html,tag):
     tag_start = html.find(start_tag)
     tag_content_start = tag_start+len(start_tag)
     tag_content_end = html.find(end_tag)
-    return html[tag_content_start:tag_content_end]
+    tag_content_raw = html[tag_content_start:tag_content_end]
+    tag_content = re.sub("<.*>", "", tag_content_raw)
+    return tag_content
     
 def get_ingredient_list(url):
     recipe_page = urlopen(url)
     html_bytes = recipe_page.read()
     html = html_bytes.decode("utf-8")
-   # print(html)
-    # title_index = html.find('<title>')
-    # title_start = title_index + len('<title>')
-    # title_end = html.find('</title>')
-    # print(html[title_start:title_end])
     title_content = get_tag_content(html,'title')
-    print(title_content)
+    ingredients_start = html.find('Ingredients')
+    directions_start = html.find('Directions')
+    ingredients_content_raw =  html[ingredients_start:directions_start]
+    #ingredients_content = re.sub("<.*>", "", ingredients_content_raw)
+    print(ingredients_content_raw)
+
+def get_ingredient_list_with_soup(url):
+    recipe_page = urlopen(url)
+    html_bytes = recipe_page.read()
+    html = html_bytes.decode("utf-8")
+    soup_html = BeautifulSoup(html, "html.parser")
+    page_text = soup_html.get_text()
+    ingredients_start = page_text.find('Ingredients')
+    directions_start = page_text.find('Directions')
+    ingredients_content_raw =  page_text[ingredients_start:directions_start]
+    print(ingredients_content_raw)
 
 url = get_url()
-get_ingredient_list(url)
+#get_ingredient_list(url)
+get_ingredient_list_with_soup(url)
